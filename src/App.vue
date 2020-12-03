46b346f72c95b328c5c727747902421509a4f346
dev-ale <ale.iphone@gmail.com>
2020-12-03 12:46:35 +0100

    remove works with message

--- src/App.vue
+++ src/App.vue
@@ -9,22 +9,26 @@
     <div id="app">
         <div class="row">
           <div class="col s12 m6 offset-m3">
-            <div v-for="gateway in gateways" :key="gateway.name" class="card blue-grey darken-1 hoverable">
-              <div class="card-content white-text">
-                <span class="card-title"><span style="font-weight: bold" class="orange-text">{{ gateway.name }}</span></span>
-                <div v-if="gateway.loading" class="progress orange accent-2">
-                  <div class="indeterminate orange accent-1"></div>
+            <h2 v-if="this.gateways.length === 0">no gateways available</h2>
+            <div v-if="this.gateways.length > 0">
+              <div v-for="gateway in gateways" :key="gateway.name" class="card blue-grey darken-1 hoverable">
+                <div class="card-content white-text">
+                  <span class="card-title"><span style="font-weight: bold" class="orange-text">{{ gateway.name }}</span></span>
+                  <div v-if="gateway.loading" class="progress orange accent-2">
+                    <div class="indeterminate orange accent-1"></div>
+                  </div>
+                  <p v-if="!gateway.loading" style="font-size: x-large">Connected Devices:</p>
+                  <p v-if="gateway.loading"style="font-size: x-large">Scanning for new Devices</p>
+                  <ul v-for="device in gateway.connectedDevices" :key="device">
+                    <li style="font-size: large">{{ device }}</li>
+                  </ul>
                 </div>
-                <p v-if="!gateway.loading" style="font-size: x-large">Connected Devices:</p>
-                <p v-if="gateway.loading"style="font-size: x-large">Scanning for new Devices</p>
-                <ul v-for="device in gateway.connectedDevices" :key="device">
-                  <li style="font-size: large">{{ device }}</li>
-                </ul>
-              </div>
-              <div class="card-action">
-                <a @click="scan(gateway)" class="waves-effect waves-light btn orange black-text">Scan for new Devices</a>
+                <div class="card-action">
+                  <a @click="scan(gateway)" class="waves-effect waves-light btn orange black-text">Scan for new Devices</a>
+                </div>
               </div>
             </div>
+
           </div>
         </div>
       </div>
@@ -95,6 +99,7 @@ export default {
           console.log('offline called')
           let i = this.gateways.findIndex(gateway => gateway.name === obj.name);
           this.gateways.splice(i, 1);
+          console.log(this.gateways)
         }
         // if online --> update values
         if(obj.online === true) {
